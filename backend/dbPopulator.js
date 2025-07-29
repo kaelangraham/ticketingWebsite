8ee4169ac68416d74a688145d6e0bac2c34e2582
kaelan <124627531+kaelangraham@users.noreply.github.com>
2025-07-29 22:28:19 +1200

    used webscraper for .csv, then made a populator program to add to db

---
+++ backend/dbPopulator.js
@@ -0,0 +1,109 @@
+const mysql = require('mysql')
+const fs = require('fs')
+const csv = require('csv-parser')
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: 'pass',
+    database: 'ticketsdb',
+    multipleStatements: true
+})
+
+const checkedGenres = []
+const checkedGenresNames = []
+const addingGenres = []
+const theatres = []
+db.query("SELECT * FROM genres; SELECT * FROM theatres; SELECT * FROM movies ORDER BY id DESC LIMIT 1", (err, data) => {
+    if(err) return err
+    if(data[1].length == 0) return
+    let latestMovieId = 0
+    if(data[2].length != 0) latestMovieId = data[2].id
+    
+    data[0].map((d) => {
+        checkedGenres.push([d.id, d.genre])
+        checkedGenresNames.push(d.genre)
+    })
+    
+    data[1].map((d) => {
+        theatres.push([d.id, d.capacity])
+    })
+
+    fs.createReadStream('movies.csv')
+        .pipe(csv())
+        .on('data', (row) => {
+
+            let genres = row.genre ? row.genre.slice(1, -1).split(',') : []
+                const id = latestMovieId + 1
+                latestMovieId = latestMovieId + 1
+
+                const name = row.name
+                const description = row.description
+                const runtime = row.runtime
+                const coverImg = row.coverImg
+                const releaseDate = row.releaseDate
+                const theatreId = Math.floor(Math.random() * (theatres.length))
+                const availableTickets = theatres[theatreId][1]
+                const totalTickets = theatres[theatreId][1]
+
+                const ticketBasePrice = 15
+                const runtimePrice = runtime[0] * 2
+                const capacityPrice = 200/totalTickets
+                
+                const ticketAdult = (ticketBasePrice + runtimePrice + capacityPrice).toFixed(2)
+                const ticketChild = (ticketBasePrice + runtimePrice + capacityPrice - 4).toFixed(2)
+                const ticketStudent = (ticketBasePrice + runtimePrice + capacityPrice - 1).toFixed(2)
+                const ticketSenior = (ticketBasePrice + runtimePrice + capacityPrice - 4).toFixed(2)
+
+                const openingHour = 9
+                const closingHour = 24
+
+                const showingTimeHour = String(Math.floor(Math.random() * (closingHour - openingHour + 1)) + openingHour).padStart(2, '0')
+                const showingTimeMinute = String(Math.floor(Math.random() * 4) * 15).padStart(2, '0')
+
+                const showingTime = `${showingTimeHour}:${showingTimeMinute}:00`
+
+                const showingDateRange = 14
+                const today = new Date()
+                const showingDateTemp = new Date(today)
+                showingDateTemp.setDate(today.getDate() + Math.floor(Math.random() * (showingDateRange + 1)))
+
+                const showingDate = showingDateTemp.toISOString().slice(0,10)
+
+                const moviesInsertSql = "INSERT INTO movies (name, description, theatreId, availableTickets, totalTickets, ticketAdult, showingTime, showingDate, runtime, coverImg, releaseDate, ticketChild, ticketStudent, ticketSenior) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                // ADD TO MOVIES
+
+                db.query(moviesInsertSql, [name, description, theatreId, availableTickets, totalTickets, ticketAdult, showingTime, showingDate, runtime, coverImg, releaseDate, ticketChild, ticketStudent, ticketSenior], (err) => {
+                    if(err) return err
+                })
+
+            genres.map((g) => {
+                let genre = JSON.parse(g).genre
+                if(!checkedGenresNames.includes(genre)) {
+                    if(checkedGenres[0]) {previousId = checkedGenres[checkedGenres.length - 1][0]} else {previousId = 0}
+                    checkedGenresNames.push(genre)
+                    checkedGenres.push([previousId + 1, genre])
+                    addingGenres.push([previousId + 1, genre])
+                }
+                const genreId = checkedGenres[checkedGenresNames.indexOf(genre)][0]
+
+                const genrerefInsertSql = "INSERT INTO genreref(movieId, genreId) VALUES (?,?)"
+                // ADD TO GENREREF
+
+                db.query(genrerefInsertSql, [id, genreId], (err) => {
+                    if(err) return err
+                })
+            })
+        
+        })
+        .on('end', () => {
+            console.log('end')
+            const genresInsertSql = "INSERT INTO genres (id, genre) VALUES (?, ?)"
+            // ADD TO GENRES
+            addingGenres.map(d => {
+                db.query(genresInsertSql, [d[0], d[1]], (err) => {
+                    if(err) return err
+                })
+            })
+        })
+})
